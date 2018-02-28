1a55bed7bc2fed20d8979f78372aee5112fb2335
Colt King <king.colt@yahoo.ca>
2018-02-28 13:01:18 -0800

    First commit

---
+++ Assignment2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment2
+//
+//  Created by Colt King on 2018-02-28.
+//  Copyright © 2018 2Toucans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
