6f27956c9e7683f32bfdc32ed987c24d44257c5b
Colt King <king.colt@yahoo.ca>
2018-03-14 15:11:11 -0700

    Added movement and rotation for easy render testing, also made progress on maze generation.

--- Assignment2/ViewController.m
+++ Assignment2/ViewController.m
@@ -15,7 +15,7 @@
 
 @implementation ViewController
 {
-    CGPoint swipePos;
+    CGPoint swipePos, rotatePos;
     Game* game;
 }
 
@@ -41,10 +41,31 @@
     }
     
     CGPoint point = [sender translationInView:self.view];
-    //translate point-swipe
-    swipePos = point;
     
+    float x = point.x - swipePos.x;
+    float y = point.y - swipePos.y;
+    
+    [game move:x y:y];
+    
+    swipePos = point;
 }
+
+- (IBAction)rotateGesture:(UIPanGestureRecognizer *)sender
+{
+    if(sender.state == UIGestureRecognizerStateBegan)
+    {
+        rotatePos = CGPointZero;
+    }
+    
+    CGPoint point = [sender translationInView:self.view];
+    
+    float x = point.x - rotatePos.x;
+    
+    [game rotate:x];
+    
+    rotatePos = point;
+}
+
 - (IBAction)doubleTap:(id)sender
 {
     //trigger console show
